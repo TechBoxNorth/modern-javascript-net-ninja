b49f363ca17ee8ff97d47180169b143b9d9c45c8
TechBoxNorth <techboxnorth@gmail.com>
2024-01-02 14:31:17 +0100

    chapter 10 - Common string methods

--- sandbox.js
+++ sandbox.js
@@ -24,3 +24,19 @@ console.log(result);
 
 let index = email.indexOf("@");
 console.log(index);
+
+// common string methods
+result = email.lastIndexOf("a");
+console.log(result);
+
+// string.slice(where to start slicing, where to stop slicing) args are inclusive
+result = email.slice(0, 10);
+console.log(result);
+
+// string.substring(where to start, how many to include)
+result = email.substring(1, 8);
+console.log(result);
+
+// string.replace('what to replace', 'what to put in')
+result = email.replace("anders", "torgny");
+console.log(result);
