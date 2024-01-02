3f04a355725b069177a770cff8ba3fa15c1d719e
TechBoxNorth <techboxnorth@gmail.com>
2024-01-02 14:11:35 +0100

    chapter 9 - Strings

---
+++ sandbox.js
@@ -0,0 +1,26 @@
+// strings
+const firstName = "Anders";
+const lastName = "Stenhammar";
+const email = "anders.stenhammar@gmail.com";
+
+// string concatenation
+let fullName = firstName + "" + lastName;
+console.log(fullName);
+
+fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// getting characters from a string
+console.log(`First letter of full name is: ${fullName[0]}`);
+
+// string length
+console.log(fullName.length);
+
+// string methods needs the opening and closing paranthesis string.Method()
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLocaleLowerCase();
+console.log(result);
+
+let index = email.indexOf("@");
+console.log(index);
