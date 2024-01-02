905d3a4886ae9f3cc8ac524fd314f3869d7c6027
TechBoxNorth <techboxnorth@gmail.com>
2024-01-02 19:56:05 +0100

    chapter 15 - booleans & comparisons

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,9 @@
-// Arrays
-let ninjas = ["leonardo", "donatello", "michelangelo", "raphael"]; // zero based array
+// booleans
 
-// overwrite an element of an array
-//ninjas[1] = "splinter";
+let email = "luigi@thenetninja.com";
+let names = ["mario", "luigi", "toad"];
 
-//console.log(ninjas);
+//let result = email.includes("!");
+//let result = names.includes("toady");
 
-// array methods
-let result = ninjas.join(" - ");
 console.log(result);
-
-result = ninjas.indexOf("raphael");
-console.log(result);
-
-// joining arrays together by concatonation
-result = ninjas.concat(["ole", "dole", "doff"]);
-console.log(result);
-
-// an element to the end of the array
-ninjas.push("bengan");
-console.log(ninjas);
-
-// remove the last element of an array
-ninjas.pop();
-console.log(ninjas);
