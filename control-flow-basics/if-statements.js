24aac2ff9d3fdc3e994166a9c7333bbf049befc5
TechBoxNorth <techboxnorth@gmail.com>
2024-01-03 15:03:54 +0100

    if, else if, else

---
+++ control-flow-basics/if-statements.js
@@ -0,0 +1,10 @@
+// if statements
+const age = 10;
+
+if (age > 20) {
+  console.log(`Yep you are older than 20!`);
+} else if (age < 20) {
+  console.log("Omg you are not old enough!");
+} else {
+  console.log(`Your age is perfect`);
+}
