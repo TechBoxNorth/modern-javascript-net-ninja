ae3b5c8b1f602d0bb245e948e1e464f566cd7fa4
TechBoxNorth <techboxnorth@gmail.com>
2024-01-03 15:46:42 +0100

    break, continue

---
+++ control-flow-basics/break-and-continue.js
@@ -0,0 +1,14 @@
+// break and continue
+
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] === 0) {
+    continue;
+  }
+  console.log("Your score: ", scores[i]);
+  if (scores[i] === 100) {
+    console.log("You got top score!");
+    break;
+  }
+}
