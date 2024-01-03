1280ea0263432033308fe62eb81a88666f0a506f
TechBoxNorth <techboxnorth@gmail.com>
2024-01-03 16:00:29 +0100

    switch statement

---
+++ control-flow-basics/switch.js
@@ -0,0 +1,26 @@
+// switch statement
+
+const grade = "C";
+
+switch (grade) {
+  case "F":
+    console.log("Study harder!");
+    break;
+  case "E":
+    console.log("You need to do better!");
+    break;
+  case "D":
+    console.log("Mediocre");
+    break;
+  case "C":
+    console.log("Good enough");
+    break;
+  case "B":
+    console.log("Very good!");
+    break;
+  case "A":
+    console.log("Excellent!");
+    break;
+  default:
+    console.log("Drop out!");
+}
