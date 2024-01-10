37de1fa418f15f28e3f8f359563a1c7fdc9b8649
TechBoxNorth <techboxnorth@gmail.com>
2024-01-10 16:49:10 +0100

    arrow functions

--- functions-and-methods/functions-methods.js
+++ functions-and-methods/functions-methods.js
@@ -15,3 +15,20 @@ console.log(result);
 subtractNumbers = (a, b) => a - b;
 sum = subtractNumbers(50, 40);
 console.log(sum);
+
+salute = () => "Hello, World!";
+let sal = salute();
+console.log(sal);
+
+let products = [10, 15, 55, 32, 54, 108, 99];
+const tax = 0.25;
+const bill = (products, tax) => {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+};
+
+let cost = bill(products, tax);
+console.log(cost);
