ef04aea48eb30d1f0113d3d8a684dfeccbeda130
TechBoxNorth <techboxnorth@gmail.com>
2024-01-15 16:05:04 +0100

    this <- is a contextual key word

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -4,10 +4,19 @@ let user = {
     age: 52,
     email: 'anders.stenhammar@gmail.com',
     location: 'Hallsberg',
+    things: ['bil', 'dator', 'mobil', 'surfplatta', 'strumpor'],
+    // method declared with the 'function' key word
     login: function(){
         console.log('The user logged in');
+    },
+    // method declared without the 'function' key word
+    logThings(){
+        this.things.forEach(thing => {
+            console.log(thing);
+        });
     }
 };
 
 console.log(user);
 user.login();
+user.logThings();
