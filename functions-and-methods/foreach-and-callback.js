95cfb2aa04a8ab0eea0716d5381e0a8ecf7d192e
TechBoxNorth <techboxnorth@gmail.com>
2024-01-15 14:35:07 +0100

    section 5: Object Literals

--- functions-and-methods/foreach-and-callback.js
+++ functions-and-methods/foreach-and-callback.js
@@ -1,39 +1,16 @@
-function valFunc(value){
-    console.log(value);
-}
-
-function myValue (valueFunction){
-    let value = 50;
-    valueFunction(value);
-};
-
-myValue(valFunc);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let people = ['mario', 'luigi', 'ryu', 'shaun', 'chin-li'];
 
 people.forEach(person => {
     console.log(person);
 });
 
-function newFunc(){
-
+function summ(a, b){
+    return a + b;
 }
+
+function addStuff(addFunc){
+    let knas = addFunc(15, 25);
+    console.log(knas);
+}
+
+addStuff(summ);
