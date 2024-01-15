8e1a470d8d2fb2e8ef816d684c7589425e4b1a73
TechBoxNorth <techboxnorth@gmail.com>
2024-01-15 14:11:20 +0100

    callback inception
    git status
    wäpRIEGHGH

---
+++ functions-and-methods/foreach-and-callback.js
@@ -0,0 +1,39 @@
+function valFunc(value){
+    console.log(value);
+}
+
+function myValue (valueFunction){
+    let value = 50;
+    valueFunction(value);
+};
+
+myValue(valFunc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chin-li'];
+
+people.forEach(person => {
+    console.log(person);
+});
+
+function newFunc(){
+
+}
