fb419f2a8df06a76cbd16460c1ea2c807ee0142a
TechBoxNorth <techboxnorth@gmail.com>
2024-01-15 16:26:57 +0100

    objects in arrays

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -5,6 +5,18 @@ let user = {
     email: 'anders.stenhammar@gmail.com',
     location: 'Hallsberg',
     things: ['bil', 'dator', 'mobil', 'surfplatta', 'strumpor'],
+    objects: [
+        {
+            name: 'stol',
+            useful: true,
+            value: 5
+        },
+        {
+            name: 'säng',
+            useful: true,
+            value: 'Priceless'
+        }
+    ],
     // method declared with the 'function' key word
     login: function(){
         console.log('The user logged in');
@@ -14,9 +26,13 @@ let user = {
         this.things.forEach(thing => {
             console.log(thing);
         });
+    },
+    logObjects(){
+        console.log(this.objects);
     }
 };
 
 console.log(user);
 user.login();
 user.logThings();
+user.logObjects();
