40e783acdd50912916be5f12236a9885041496a3
TechBoxNorth <techboxnorth@gmail.com>
2024-01-15 17:16:27 +0100

    Primitive Types vs Reference Types

---
+++ object-literals/primitive-vs-reference-types.js
@@ -0,0 +1,19 @@
+// ------------------ Primitive & Reference Types -------------------
+
+/* Primitive Types:
+-numbers
+-strings
+-booleans
+-null
+-undefined
+-symbols
+*/
+
+/* Reference Types:
+-all types of objects
+ -object literals
+ -arrays
+ -functions
+ -dates
+ -all other objects
+*/
