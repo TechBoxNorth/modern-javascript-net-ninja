caaf81250426749f7011f918b89aab6dd0a0420b
TechBoxNorth <techboxnorth@gmail.com>
2024-01-18 14:33:25 +0100

    getElementBy -Id, -ClassName, -TagName

--- The Document Object Model/document-object-model.js
+++ The Document Object Model/document-object-model.js
@@ -1,5 +1,24 @@
+/*
+
+// query selector
 const para = document.querySelector('div.error');
 console.log(para);
 
 const paras = document.querySelectorAll('p');
+console.log(paras);
+
+*/
+
+// get an element ny ID
+const title = document.getElementById('page-title');
+console.log(title);
+
+
+// get elements by their class name
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+
+
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
 console.log(paras);
